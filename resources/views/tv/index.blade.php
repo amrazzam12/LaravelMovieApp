f5f2dfab65b18af6cb97a130b4231df0c18e4e43
amrazzam12 <you@example.com>
2022-03-21 02:44:50 +0200

    updated

--- resources/views/tv/index.blade.php
+++ resources/views/tv/index.blade.php
@@ -7,7 +7,6 @@
             <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Top Rated Shows</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
                 @foreach($popularShows as $show)
-
                     <x-tvshow :show="$show" :genres="$genres" />
                 @endforeach
             </div>
